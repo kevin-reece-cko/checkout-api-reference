ee189a462b692f2d6561d484477fdb437fc07e6e
KevinReece <kevin.reece@checkout.com>
2019-01-15 16:48:43 +0100

    APL-246 - Sync Klarna response

--- scripts/sync-generated-specs.js
+++ scripts/sync-generated-specs.js
@@ -9,42 +9,49 @@ set('-v');
 
 var syncPaymentSources = function()
 {
-  syncPaymentSource('Klarna', 'http://qa-gateway-internal.cko.lon/klarna-internal/relations/gw/pay', '14');
+  syncPaymentRequest('Klarna', 'http://qa-gateway-internal.cko.lon/klarna-internal/relations/gw/pay', '14');
+  syncPaymentResponse('Klarna', 'http://qa-gateway-internal.cko.lon/klarna-internal/relations/gw/pay', '10');
 }
 
-var syncPaymentSource = function(paymentSourceName, paymentSpecUrl, outputFilePrefix) {
-  console.log('Requesting ' + paymentSourceName + ' payment spec from: ' + paymentSpecUrl);
+var syncPaymentRequest = function(paymentSourceName, paymentSpecUrl, outputFilePrefix) {
   var outputFilename =  outputFilePrefix + '_PaymentRequest' + paymentSourceName + 'Source.yaml'
   var outputFilePath = 'spec/components/schemas/Payments/RequestSources/' + outputFilename;
-  var buildOutputFunction = getFunctionToBuildPaymentSourceYaml(paymentSourceName);
+  var buildPaymentRequestFunction = getFunctionToBuildPaymentRequest(paymentSourceName);
+  sync(paymentSpecUrl, buildPaymentRequestFunction, outputFilePath);
+};
+
+var syncPaymentResponse = function(paymentSourceName, paymentSpecUrl, outputFilePrefix) {
+  var outputFilename =  outputFilePrefix + '_PaymentResponse' + paymentSourceName + 'Source.yaml'
+  var outputFilePath = 'spec/components/schemas/Payments/ResponseSources/' + outputFilename;
+  var buildPaymentResponseFunction = getFunctionToBuildPaymentResponse(paymentSourceName);
+  sync(paymentSpecUrl, buildPaymentResponseFunction, outputFilePath);
+};
+
+var sync = function(paymentSpecUrl, buildPaymentResponseFunction, outputFilePath) {
+  console.log('Requesting payment spec from: ' + paymentSpecUrl + ', to output: ' + outputFilePath);
+  var buildOutputFunction = getFunctionToBuildYaml(buildPaymentResponseFunction);
   request({ url: paymentSpecUrl, json: true}, thenBuildOutputAndWrite(buildOutputFunction, outputFilePath));
 };
 
-var getFunctionToBuildPaymentSourceYaml = function(paymentSourceName) {
+var getFunctionToBuildPaymentRequest = function(paymentSourceName) {
   return function(responseBody) {
     var requestData = responseBody.put.requestBody.content['application/json'].schema.properties.request_data;
-    var builtPaymentSource = buildPaymentSource(requestData, paymentSourceName);
-    var generatedFileWarningComment = '###\n# Warning: this file was auto generated from OpenAPI specs using \'npm run sync-generated-specs\'. Do not manually edit. \n###\n';
-    return generatedFileWarningComment + YAML.stringify(builtPaymentSource, 20, 2);
-  };
-};
-
-var buildPaymentSource = function(requestData, paymentSourceName) {
-  if (requestData.properties.type) {
-    delete requestData.properties.type;
-  }
-  addDescriptionToKlarnaPassthroughObjects(requestData, paymentSourceName);
-  return {
-    type: 'object',
-    description: paymentSourceName + ' Source',
-    allOf: [{
-        $ref: '#/components/schemas/PaymentRequestSource',
-      }, {
-        type: 'object',
-        required: requestData.required,
-        properties: requestData.properties
-      }
-    ]
+    if (requestData.properties.type) {
+      delete requestData.properties.type;
+    }
+    addDescriptionToKlarnaPassthroughObjects(requestData, paymentSourceName);
+    return {
+      type: 'object',
+      description: paymentSourceName + ' Source',
+      allOf: [{
+          $ref: '#/components/schemas/PaymentRequestSource',
+        }, {
+          type: 'object',
+          required: requestData.required,
+          properties: requestData.properties
+        }
+      ]
+    };
   };
 };
 
@@ -58,6 +65,34 @@ var addDescriptionToKlarnaPassthroughObjects = function(requestData, paymentSour
                                 so for the object definition use the [' + paymentSourceName + ' documentation](' + apmPropertyDocs + ').';
     }
   });
+};
+
+var getFunctionToBuildPaymentResponse = function(paymentSourceName) {
+  return function(responseBody) {
+    var requestData = responseBody.put.responses['201'].content['application/json'].schema.properties.response_data;
+    if (requestData.properties.type) {
+      delete requestData.properties.type;
+    }
+    return {
+      type: 'object',
+      description: paymentSourceName + ' Source',
+      allOf: [{
+          $ref: '#/components/schemas/PaymentResponseSource',
+        }, {
+          type: 'object',
+          properties: requestData.properties
+        }
+      ]
+    };
+  };
+};
+
+var getFunctionToBuildYaml = function(buildOutputFunction) {
+  return function(responseBody) {
+    var builtOutput = buildOutputFunction(responseBody);
+    var generatedFileWarningComment = '###\n# Warning: this file was auto generated from OpenAPI specs using \'npm run sync-generated-specs\'. Do not manually edit. \n###\n';
+    return generatedFileWarningComment + YAML.stringify(builtOutput, 20, 2);
+  };
 };
 
 var thenBuildOutputAndWrite = function(buildOutputFunction, outputFilePath) {
