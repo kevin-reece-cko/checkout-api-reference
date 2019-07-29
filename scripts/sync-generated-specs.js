cee0c17b66b72f733316a9cbdde2f118fac44e3d
Adam Jasinski <adam.jasinski@checkout.com>
2019-07-29 17:01:37 +0200

    P24 specs

--- scripts/sync-generated-specs.js
+++ scripts/sync-generated-specs.js
@@ -23,6 +23,8 @@ var syncPaymentSources = function()
   syncPaymentResponse('Bancontact', 'http://sb-gateway-internal.cko.lon/ppro-internal/bancontact/relations/gw/payment');
   syncPaymentRequest('Fawry', 'http://sb-gateway-internal.cko.lon/fawry-internal/relations/gw/pay');
   syncPaymentResponse('Fawry', 'http://sb-gateway-internal.cko.lon/fawry-internal/relations/gw/payment');
+  syncPaymentRequest('P24', 'http://sb-gateway-internal.cko.lon/ppro-internal/p24/relations/gw/pay');
+  syncPaymentResponse('P24', 'http://sb-gateway-internal.cko.lon/ppro-internal/p24/relations/gw/payment');
 }
 
 var syncPaymentRequest = function(paymentSourceName, paymentSpecUrl) {
