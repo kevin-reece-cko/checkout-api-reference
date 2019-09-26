cd82b16ecef46daa733c92ba9f8758d13b831f69
KevinReece <kevin.reece@checkout.com>
2019-09-26 16:12:22 +0200

    BEN-32 - BenefitPay request/response

--- scripts/sync-generated-specs.js
+++ scripts/sync-generated-specs.js
@@ -27,6 +27,8 @@ var syncPaymentSources = function()
   syncPaymentResponse('QPay', 'http://sb-gateway-internal.cko.lon/qpay-internal/relations/gw/payment');
   syncPaymentRequest('P24', 'http://sb-gateway-internal.cko.lon/ppro-internal/p24/relations/gw/pay');
   syncPaymentResponse('P24', 'http://sb-gateway-internal.cko.lon/ppro-internal/p24/relations/gw/payment');
+  syncPaymentRequest('BenefitPay', 'http://sb-gateway-internal.cko.lon/benefitpay-internal/relations/gw/pay');
+  syncPaymentResponse('BenefitPay', 'http://sb-gateway-internal.cko.lon/benefitpay-internal/relations/gw/payment');
 }
 
 var syncPaymentRequest = function(paymentSourceName, paymentSpecUrl) {
