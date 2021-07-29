10a197d3f3bb3f5c1dad4d9b2231628e3422f585
Jack Hutchinson <jack.hutchinson@checkout.com>
2021-07-29 18:03:35 +0100

    Add gcdc script to html head

---
+++ web/js/gcdc.js
@@ -0,0 +1,8 @@
+(function(g,a,t,e,d,c,o){
+    if (!g[d]) {g.GatedContentObject=d;
+    g[d]=g[d]||function(){(g[d].q=g[d].q||[]).push(arguments)};
+    c=a.createElement(t),o=a.getElementsByTagName(t)[0];
+    c.async=1;c.src=e;o.parentNode.insertBefore(c, o)}
+})(window, document, 'script', 'https://app.gatedcontent.com/scripts/29853457/app.js', 'gcdc');
+gcdc('loadGates');
+
