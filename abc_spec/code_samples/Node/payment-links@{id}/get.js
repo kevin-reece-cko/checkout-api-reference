72c71f51e97d1992a17a8d66e439a166715d2fbb
chrisi-webster-cko <77628673+chrisi-webster-cko@users.noreply.github.com>
2021-08-10 10:54:43 +0100

    Update Plinks Node example

--- abc_spec/code_samples/Node/payment-links@{id}/get.js
+++ abc_spec/code_samples/Node/payment-links@{id}/get.js
@@ -2,4 +2,4 @@ import { Checkout } from 'checkout-node-sdk';
 
 const cko = new Checkout('sk_XXXX');
 
-const payment = await cko.payments.get('pl_irx_SMlY5RCA');
+const payment = await cko.paymentsLinks.get('pl_irx_SsMlY5RCA');
