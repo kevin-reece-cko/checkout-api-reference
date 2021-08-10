844a2f03e1e374977e0267305c92ba4d32c2bc2a
chrisi-webster-cko <77628673+chrisi-webster-cko@users.noreply.github.com>
2021-08-10 10:56:47 +0100

    Update get.js

--- abc_spec/code_samples/Node/payment-links@{id}/get.js
+++ abc_spec/code_samples/Node/payment-links@{id}/get.js
@@ -2,4 +2,4 @@ import { Checkout } from 'checkout-node-sdk';
 
 const cko = new Checkout('sk_XXXX');
 
-const payment = await cko.paymentsLinks.get('pl_irx_SsMlY5RCA');
+const payment = await cko.paymentLinks.get('pl_irx_SsMlY5RCA');
