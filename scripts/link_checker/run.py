33c20e6011d81093a03ca0bf284672f3eb854f59
Ben Ahmady <ben.ahmady@checkout.com>
2022-02-22 13:43:33 +0000

    Fail build if broken links found

--- scripts/link_checker/run.py
+++ scripts/link_checker/run.py
@@ -1,13 +1,23 @@
 from link_checker import check_broken_links
+import sys
+
+BROKEN_LINKS_EXIST = False
 
 print("ABC broken links", sep="\n")
 print("-----------------", sep="\n")
 for i in check_broken_links("abc"):
     print(i)
+    if i:
+        BROKEN_LINKS_EXIST = True
 print("-----------------", sep="\n")
 
 print("NAS broken links", sep="\n")
 print("-----------------", sep="\n")
 for i in check_broken_links("nas"):
     print(i)
+    if i:
+        BROKEN_LINKS_EXIST = True
 print("-----------------", sep="\n")
+
+if BROKEN_LINKS_EXIST:
+    sys.exit(1)
